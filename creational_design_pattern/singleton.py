4c7ab83f05a054e43ea8473ca235f54cfc4f207e
Guruprasad <Guruprasad>
2026-02-02 22:34:13 +0530

    Double check method in singleton threading

--- creational_design_pattern/singleton.py
+++ creational_design_pattern/singleton.py
@@ -40,22 +40,44 @@ class LazySingleton():
 
 # Several ways to make Singleton thread safe
 # we can prevent multiple threads from creating multiple instances
-# 1. Synchronised method 
 # threading.Lock() is a mutex (mutual exclusion lock).
 # It is a key that allows only one thread at a time to enter a critical section of code.
-import threading
-class Singleton:
-    __instance =None
-    __lock=threading.Lock()
 
+# 1. Synchronised method 
+# import threading
+# class Singleton:
+#     __instance =None
+#     __lock=threading.Lock()
+
+#     def __init__(self):
+#         if Singleton.__instance is not None:
+#             raise Exception("This class is singleton")
+#         Singleton.__instance=self
+
+#     @staticmethod
+#     def getInstance():
+#         with Singleton.__lock:
+#             if Singleton.__instance is  None:
+#                 Singleton()
+#             return Singleton.__instance
+
+
+#Double checking method
+import threading 
+class Singleton:
+    __instance=None
+    _lock=threading.Lock()
     def __init__(self):
         if Singleton.__instance is not None:
-            raise Exception("This class is singleton")
+            raise Exception("this is singleton") 
         Singleton.__instance=self
-
     @staticmethod
     def getInstance():
-        with Singleton.__lock:
-            if Singleton.__instance is not None:
-                Singleton()
-            return Singleton.__instance
+        if Singleton.__instance is None:
+            with Singleton._lock:
+                if Singleton.__instance is None:
+                    Singleton()
+        return Singleton.__instance
+    
+
+print(Singleton.getInstance())
